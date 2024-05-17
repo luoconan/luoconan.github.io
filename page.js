fbc81251e3960679f8fb98394ac2eeb82120c783
luoconan <161291744+luoconan@users.noreply.github.com>
2024-05-16 22:30:01 -0700

    Update page.js

--- page.js
+++ page.js
@@ -3,6 +3,7 @@ const PAGES = [
     ["newDar.html", "create the trip code"],
     ["prts_info.js", "Data For Prts"],
     ["OneHourViolation.html","build csv file to set 1 hour violation"],
+    ["createTable.html","Create whole Table for External and DAR"],
     ["test.html", "test page. "],
     ["page.js","page config"]
 ];
