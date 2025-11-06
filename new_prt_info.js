bbfdc6418bd50071ef606817023fa7860b6e8729
luoconan <161291744+luoconan@users.noreply.github.com>
2025-11-05 16:15:53 -0800

    Update new_prt_info.js

--- new_prt_info.js
+++ new_prt_info.js
@@ -1,4 +1,4 @@
-//version 10/21/2025 15:15/
+//version 11/5/2025 15:15/
 addrs={
         "1 Thomas More Way": "2",
         "1001 Franklin Street": "8",
@@ -185,7 +185,954 @@ addrs={
         "966 Randolph Street": "2",
         "995 Brussels Street": "6"
     }
-
+prtObjInfo = {
+    "Abele, Arija": {
+        "room": 3
+    },
+    "Abesamis, Purificacion K": {
+        "room": 3
+    },
+    "Aldana, Eugenia Alberta": {
+        "room": 2
+    },
+    "Allen, Jerre T": {
+        "room": 3
+    },
+    "Amezcua, Margaret M": {
+        "room": 3
+    },
+    "Anies, Cristeta": {
+        "room": 2
+    },
+    "Aubrey, Corrine": {
+        "room": 3
+    },
+    "Auyeung, Shum K": {
+        "room": 2
+    },
+    "Auyoung, Tim": {
+        "room": 1
+    },
+    "Beck, Ora": {
+        "room": 3
+    },
+    "Bello, Christopher": {
+        "room": 2
+    },
+    "Bishop Robinson, Jacqueline": {
+        "room": 3
+    },
+    "Bolden, Joseph": {
+        "room": 3
+    },
+    "Bonilla, Yolanda": {
+        "room": 3
+    },
+    "Bonner, Patricia": {
+        "room": 2
+    },
+    "Bray, Georgina  Margaret": {
+        "room": 3
+    },
+    "Briggs, Sharon": {
+        "room": 3
+    },
+    "Broadnax, Eugene": {
+        "room": 3
+    },
+    "Bunag, Imelda": {
+        "room": 3
+    },
+    "Bunag, Relio": {
+        "room": 3
+    },
+    "Cabrera de Interiano, Elva": {
+        "room": 3
+    },
+    "Cafferty, Ellen F": {
+        "room": 3
+    },
+    "Campbell, Steph": {
+        "room": 3
+    },
+    "Canizalez, Graciela": {
+        "room": 3
+    },
+    "Carroll, Robert A": {
+        "room": 3
+    },
+    "Carroll-Iturralde, Ruth": {
+        "room": 3
+    },
+    "Cassidy, Betty": {
+        "room": 2
+    },
+    "Chan, Albert N": {
+        "room": 3
+    },
+    "Chan, David L": {
+        "room": 1
+    },
+    "Chan, Elaine": {
+        "room": 1
+    },
+    "Chan, Kwan Har": {
+        "room": 1
+    },
+    "Chan, Patricia": {
+        "room": 1
+    },
+    "Chan, Phillip": {
+        "room": 1
+    },
+    "Chan, Shun Chow": {
+        "room": 1
+    },
+    "Chan, Sook Wah": {
+        "room": 1
+    },
+    "Chan, Suk Ching": {
+        "room": 2
+    },
+    "Chan, Sylvia W": {
+        "room": 1
+    },
+    "Chan, Virginia C": {
+        "room": 1
+    },
+    "Chau, Polo": {
+        "room": 1
+    },
+    "Chau, Simon": {
+        "room": 1
+    },
+    "Chavez Lopez, Humberto": {
+        "room": 2
+    },
+    "Chen, Annie": {
+        "room": 1
+    },
+    "Chen, Buoy Lan": {
+        "room": 2
+    },
+    "Chen, Shun Hui": {
+        "room": 1
+    },
+    "Chen, Yiqun": {
+        "room": 1
+    },
+    "Chen, Zong Liang": {
+        "room": 2
+    },
+    "Chen, Zuo Ren": {
+        "room": 1
+    },
+    "Cheng, Guang Shun": {
+        "room": 1
+    },
+    "Cheng, James": {
+        "room": 2
+    },
+    "Cheung, William": {
+        "room": 2
+    },
+    "Chiang, Kuo Hsiun": {
+        "room": 1
+    },
+    "Chin, Ang Lin": {
+        "room": 1
+    },
+    "Chin, Anita": {
+        "room": 1
+    },
+    "Chou, Sio Cheong": {
+        "room": 1
+    },
+    "Chow, James": {
+        "room": 2
+    },
+    "Chow, Wan Chung": {
+        "room": 2
+    },
+    "Chow, Yau Soong": {
+        "room": 1
+    },
+    "Chow, Yuliang": {
+        "room": 1
+    },
+    "Chryst, Genn": {
+        "room": 3
+    },
+    "Chu, Clyde Y": {
+        "room": 1
+    },
+    "Chu, Kenneth K": {
+        "room": 1
+    },
+    "Chu, Kwai Lan": {
+        "room": 1
+    },
+    "Chu, Yin": {
+        "room": 1
+    },
+    "Chung, Ann": {
+        "room": 1
+    },
+    "Chung, Isaac I": {
+        "room": 2
+    },
+    "Chung, Suk W": {
+        "room": 2
+    },
+    "Cornel, Rebecca": {
+        "room": 3
+    },
+    "Cruz, Ruben D": {
+        "room": 2
+    },
+    "Dang, Muoi": {
+        "room": 1
+    },
+    "Dang, Tony": {
+        "room": 1
+    },
+    "Dao, Ngoc Tu": {
+        "room": 2
+    },
+    "Dao, Nha Binh": {
+        "room": 1
+    },
+    "Dao, Tran Tu": {
+        "room": 2
+    },
+    "Deguzman, Evangelina": {
+        "room": 2
+    },
+    "Dokko, Kyungwon": {
+        "room": 3
+    },
+    "Doyle, Tom": {
+        "room": 3
+    },
+    "Duong, Ban": {
+        "room": 1
+    },
+    "Fan, Hongyu": {
+        "room": 1
+    },
+    "Fang, Yue Hua": {
+        "room": 1
+    },
+    "Ferrell, Wanda": {
+        "room": 3
+    },
+    "Fischer, Felizitas M": {
+        "room": 3
+    },
+    "Fox, Steven W.": {
+        "room": 3
+    },
+    "Fung, Lai King": {
+        "room": 2
+    },
+    "Fung, Yuk Leung": {
+        "room": 1
+    },
+    "Gallegos Figeroa, Adriana": {
+        "room": 3
+    },
+    "Gao, Li Ping": {
+        "room": 1
+    },
+    "Garcia Maldonado, Aurelia": {
+        "room": 3
+    },
+    "Garcia, Agustin": {
+        "room": 3
+    },
+    "Garcia, Dora J": {
+        "room": 3
+    },
+    "Godinez, Luis": {
+        "room": 2
+    },
+    "Green, Jenny": {
+        "room": 3
+    },
+    "Gu, Guan Qun": {
+        "room": 1
+    },
+    "Guerra Ayala, Alma": {
+        "room": 3
+    },
+    "Guevara, Judith Teresa": {
+        "room": 2
+    },
+    "Harris, Janet": {
+        "room": 2
+    },
+    "Hau, Yuet": {
+        "room": 1
+    },
+    "Haynes, Cristina": {
+        "room": 3
+    },
+    "He, Yu": {
+        "room": 1
+    },
+    "Head, Sherrie": {
+        "room": 2
+    },
+    "Hellam, Ruben A": {
+        "room": 3
+    },
+    "Helms, John F": {
+        "room": 3
+    },
+    "Hernandez, Marcia": {
+        "room": 3
+    },
+    "Hernandez, Victoria": {
+        "room": 3
+    },
+    "Ho, Anna": {
+        "room": 1
+    },
+    "Ho, Kim Yee": {
+        "room": 2
+    },
+    "Ho, Sandy": {
+        "room": 2
+    },
+    "Horne, Ronald W": {
+        "room": 3
+    },
+    "Huang Tam, Gui Ying": {
+        "room": 1
+    },
+    "Huang, Chien Chien": {
+        "room": 3
+    },
+    "Huang, Jing Hua": {
+        "room": 1
+    },
+    "Huang, Qi Chang": {
+        "room": 1
+    },
+    "Huang, Rui Wen": {
+        "room": 1
+    },
+    "Huang, Shao Ming": {
+        "room": 1
+    },
+    "Huang, Shu Fen": {
+        "room": 1
+    },
+    "Huang, Wei Liang": {
+        "room": 1
+    },
+    "Huang, Wendy W": {
+        "room": 1
+    },
+    "Huang, Ying J": {
+        "room": 1
+    },
+    "Huang, YongLin": {
+        "room": 1
+    },
+    "Hubbard, Charlotte": {
+        "room": 3
+    },
+    "Hughes, Spencer": {
+        "room": 3
+    },
+    "Hui, Wai Chun Ngai": {
+        "room": 1
+    },
+    "Hung, Mei Sheung": {
+        "room": 1
+    },
+    "Huynh, Hi": {
+        "room": 1
+    },
+    "Huynh, Muoi Trieu T": {
+        "room": 1
+    },
+    "Hwang, Soon": {
+        "room": 2
+    },
+    "Jin, Xue Lian": {
+        "room": 1
+    },
+    "Johnson, Willie J": {
+        "room": 3
+    },
+    "Jones, Elaine": {
+        "room": 3
+    },
+    "Kerfoot, Lori J": {
+        "room": 2
+    },
+    "Keung, Ping Wan": {
+        "room": 2
+    },
+    "Kim, Lily": {
+        "room": 1
+    },
+    "Kwong Chang, Lin Hing": {
+        "room": 1
+    },
+    "Kwong, Chi Min": {
+        "room": 1
+    },
+    "Kwong, Yuk Lin Ng": {
+        "room": 1
+    },
+    "Lai, Wai Ping": {
+        "room": 1
+    },
+    "Lam, Thomas": {
+        "room": 1
+    },
+    "Lau, Cheng Kun": {
+        "room": 1
+    },
+    "Lau, Kitwan": {
+        "room": 1
+    },
+    "Lee, Alydia": {
+        "room": 2
+    },
+    "Lee, Anna": {
+        "room": 1
+    },
+    "Lee, Byoung Ju": {
+        "room": 3
+    },
+    "Lee, Choi Kuen": {
+        "room": 1
+    },
+    "Lee, Fung": {
+        "room": 2
+    },
+    "Lee, FungLin So": {
+        "room": 1
+    },
+    "Lee, Gertrude": {
+        "room": 1
+    },
+    "Lee, Har Tam": {
+        "room": 2
+    },
+    "Lee, Lai Yee": {
+        "room": 1
+    },
+    "Lee, Lap Chow": {
+        "room": 1
+    },
+    "Lee, May Jean J": {
+        "room": 1
+    },
+    "Lee, Mui Chun": {
+        "room": 2
+    },
+    "Lee, Ngan Chu": {
+        "room": 1
+    },
+    "Lee, Shuk F Lee": {
+        "room": 1
+    },
+    "Lee, Sooja": {
+        "room": 2
+    },
+    "Lee-Danridge, Robin E": {
+        "room": 3
+    },
+    "Leung, Frank F": {
+        "room": 2
+    },
+    "Leung, Hoi Man": {
+        "room": 1
+    },
+    "Leung, Hung Chun": {
+        "room": 1
+    },
+    "Leung, Leisly L": {
+        "room": 2
+    },
+    "Leung, Ricky H": {
+        "room": 3
+    },
+    "Li, Kewu": {
+        "room": 1
+    },
+    "Li, Yan Zhi": {
+        "room": 2
+    },
+    "Li, Zhi Yuan": {
+        "room": 1
+    },
+    "Liang, Guo Zhi": {
+        "room": 1
+    },
+    "Liang, Huan Mei": {
+        "room": 1
+    },
+    "Liang, Minan": {
+        "room": 1
+    },
+    "Liang, Sai Huan": {
+        "room": 1
+    },
+    "Lin, Chao Ping": {
+        "room": 1
+    },
+    "Lin, Phyllis": {
+        "room": 2
+    },
+    "Liu, Kuo Liang": {
+        "room": 1
+    },
+    "Lo, To Chai": {
+        "room": 1
+    },
+    "Logan, Venitta": {
+        "room": 3
+    },
+    "Lu, Linda": {
+        "room": 1
+    },
+    "Lu, Xiong": {
+        "room": 2
+    },
+    "Luk Chiu, Fung Hin": {
+        "room": 2
+    },
+    "Lum, Yung Ching": {
+        "room": 1
+    },
+    "Lung, Dar Tsai": {
+        "room": 1
+    },
+    "Lung, Jean H": {
+        "room": 1
+    },
+    "Luong, Lau A": {
+        "room": 2
+    },
+    "Luong, Vu Q": {
+        "room": 1
+    },
+    "Luu, Muoi": {
+        "room": 1
+    },
+    "Luu, To": {
+        "room": 1
+    },
+    "Ly, Ha Bich": {
+        "room": 1
+    },
+    "Ly, Mui": {
+        "room": 2
+    },
+    "Ly, Trung": {
+        "room": 1
+    },
+    "Lyons, Gregory B.": {
+        "room": 3
+    },
+    "Ma, Cui Hua": {
+        "room": 1
+    },
+    "Ma, Helen": {
+        "room": 1
+    },
+    "Ma, Rongan": {
+        "room": 1
+    },
+    "Mac, Tammy Thuy": {
+        "room": 1
+    },
+    "MacDonald, Christopher": {
+        "room": 3
+    },
+    "Manilla, Danilo": {
+        "room": 2
+    },
+    "Martinez, Lawrence Benedict": {
+        "room": 3
+    },
+    "Matosich, Helen A": {
+        "room": 2
+    },
+    "Matsuo, Emiko": {
+        "room": 2
+    },
+    "McCarthy, Steven": {
+        "room": 3
+    },
+    "Mederos, Aquileo": {
+        "room": 3
+    },
+    "Mei, Elizabeth": {
+        "room": 3
+    },
+    "Melville, Ruth": {
+        "room": 3
+    },
+    "Mengesha, Hiwan": {
+        "room": 3
+    },
+    "Miao, Bao Fei": {
+        "room": 1
+    },
+    "Miller, Marvin": {
+        "room": 3
+    },
+    "Miller, Thomas": {
+        "room": 3
+    },
+    "Montoya, Luis Alonzo": {
+        "room": 3
+    },
+    "Mosier, Jessica": {
+        "room": 2
+    },
+    "Moskvychov, Svyatoslav G": {
+        "room": 3
+    },
+    "Mui, So Shung": {
+        "room": 1
+    },
+    "Nagata, Bulou V": {
+        "room": 3
+    },
+    "Ng, Kwai": {
+        "room": 1
+    },
+    "Ng, Ping Jan": {
+        "room": 1
+    },
+    "Ng, Siu H Yu": {
+        "room": 1
+    },
+    "Ng, Wah Chi": {
+        "room": 1
+    },
+    "Nudelman, Irina": {
+        "room": 3
+    },
+    "Nunn, Sue Tsin": {
+        "room": 1
+    },
+    "Ornano, Julia": {
+        "room": 3
+    },
+    "Pal, Gary": {
+        "room": 3
+    },
+    "Pascal, Fabian": {
+        "room": 3
+    },
+    "Pawaskar, Kavita": {
+        "room": 3
+    },
+    "Pawaskar, Kumar": {
+        "room": 3
+    },
+    "Peed, Leatrice": {
+        "room": 2
+    },
+    "Penry, Paul E": {
+        "room": 2
+    },
+    "Perkins, Michael C": {
+        "room": 2
+    },
+    "Peterson, Samuel": {
+        "room": 2
+    },
+    "Pho, Lina": {
+        "room": 1
+    },
+    "Poon, Man Sin Wong": {
+        "room": 1
+    },
+    "Poon, Ming Hoi": {
+        "room": 1
+    },
+    "Pugh, Michael": {
+        "room": 3
+    },
+    "Quach, Miles": {
+        "room": 1
+    },
+    "Ramos, Gladys": {
+        "room": 3
+    },
+    "Randleston, Edward C.": {
+        "room": 3
+    },
+    "Rapisura, Herminia": {
+        "room": 3
+    },
+    "Richardson, Mark L": {
+        "room": 2
+    },
+    "Rich-French, JennyLynn": {
+        "room": 3
+    },
+    "Rodriguez, Helen": {
+        "room": 3
+    },
+    "Rothstein, Karen": {
+        "room": 3
+    },
+    "Salunga, Marissa": {
+        "room": 3
+    },
+    "Savage, Nicole": {
+        "room": 3
+    },
+    "Shahib, Anita": {
+        "room": 3
+    },
+    "Siu, Wah Bik B": {
+        "room": 2
+    },
+    "Smiley, Patricia": {
+        "room": 2
+    },
+    "Sorrells, Bryan": {
+        "room": 3
+    },
+    "Stamps, Ivory": {
+        "room": 3
+    },
+    "Sun, Ruilian": {
+        "room": 2
+    },
+    "Swimmer, John": {
+        "room": 2
+    },
+    "Takikawa, Noriko": {
+        "room": 2
+    },
+    "Tam, Sin King": {
+        "room": 1
+    },
+    "Tam, Siu C": {
+        "room": 1
+    },
+    "Tam, Tsui L": {
+        "room": 1
+    },
+    "Tam, Wai Ken": {
+        "room": 1
+    },
+    "Tan, Raymond": {
+        "room": 1
+    },
+    "Tang, Kok Fang": {
+        "room": 1
+    },
+    "Taylor, Peggy": {
+        "room": 2
+    },
+    "Terry, Darlene A": {
+        "room": 3
+    },
+    "The, Siang Hoei": {
+        "room": 3
+    },
+    "Todd, Terrence Donald": {
+        "room": 3
+    },
+    "Tran, Anh": {
+        "room": 1
+    },
+    "Tran, Hong Thu": {
+        "room": 1
+    },
+    "Tran, Lien T": {
+        "room": 1
+    },
+    "Tran, Phuoc": {
+        "room": 2
+    },
+    "Tse, Ying Lam": {
+        "room": 1
+    },
+    "Tu, David": {
+        "room": 1
+    },
+    "Umeukeje, Alice": {
+        "room": 2
+    },
+    "Vaccari, Vivian": {
+        "room": 3
+    },
+    "Vasquez, Leticia": {
+        "room": 2
+    },
+    "Vass, Tatiana": {
+        "room": 2
+    },
+    "Vu, Thuy": {
+        "room": 1
+    },
+    "Wan, Lai Yin": {
+        "room": 1
+    },
+    "Wang Chang, Fung Hwa": {
+        "room": 1
+    },
+    "Wang, Caishao": {
+        "room": 1
+    },
+    "Wang, Chun Ling": {
+        "room": 1
+    },
+    "Wang, Dao Gang": {
+        "room": 1
+    },
+    "Wang, Judy W": {
+        "room": 1
+    },
+    "Wang, Xiaohua": {
+        "room": 1
+    },
+    "Wang, You Lan": {
+        "room": 1
+    },
+    "Washington, Betty": {
+        "room": 3
+    },
+    "Watt, Kwok Yan Hong": {
+        "room": 1
+    },
+    "Wernick, Sandra": {
+        "room": 3
+    },
+    "Williams, Michael": {
+        "room": 3
+    },
+    "Wilton, Donald R": {
+        "room": 2
+    },
+    "Wong, Bill Kwok": {
+        "room": 1
+    },
+    "Wong, Fung Ying": {
+        "room": 1
+    },
+    "Wong, Gam Fee": {
+        "room": 2
+    },
+    "Wong, Henry H": {
+        "room": 1
+    },
+    "Wong, Iris": {
+        "room": 1
+    },
+    "Wong, Lan Ying": {
+        "room": 2
+    },
+    "Wong, Mangyung": {
+        "room": 1
+    },
+    "Wong, May Oi": {
+        "room": 1
+    },
+    "Wong, May Seung": {
+        "room": 1
+    },
+    "Wong, Shou Chu": {
+        "room": 1
+    },
+    "Woo, May": {
+        "room": 1
+    },
+    "Woo, Mu Jie Xie": {
+        "room": 1
+    },
+    "Woods, Renita": {
+        "room": 3
+    },
+    "Wu, Eva Ying Fong": {
+        "room": 1
+    },
+    "Xu, Anne": {
+        "room": 1
+    },
+    "Xu, Gen Su": {
+        "room": 1
+    },
+    "Xu, Ziyin": {
+        "room": 1
+    },
+    "Yan, Xin Fa": {
+        "room": 1
+    },
+    "Yang, Feng Lan": {
+        "room": 1
+    },
+    "Yao, MeiXian": {
+        "room": 1
+    },
+    "Ye, Hui Juan": {
+        "room": 2
+    },
+    "Ye, Wei Hua": {
+        "room": 1
+    },
+    "Ye, Yibin": {
+        "room": 1
+    },
+    "Yeoh, Khian Heng": {
+        "room": 1
+    },
+    "Yeung, Linying L": {
+        "room": 1
+    },
+    "Yim, Wai Hing": {
+        "room": 1
+    },
+    "Yu, Rui Guo": {
+        "room": 2
+    },
+    "Yu, Thomas": {
+        "room": 2
+    },
+    "Yu, Yong Dong": {
+        "room": 1
+    },
+    "Yuan, Fu Mei": {
+        "room": 1
+    },
+    "Yuan, Shun": {
+        "room": 1
+    },
+    "Yuen, Helen S": {
+        "room": 1
+    },
+    "Yuen, Richard S": {
+        "room": 1
+    },
+    "Zeng, Hanying": {
+        "room": 1
+    },
+    "Zhang, De Zhen": {
+        "room": 1
+    },
+    "Zhou, Xing Hua": {
+        "room": 1
+    },
+    "Zhu, Cui Liang": {
+        "room": 1
+    }
+}
+/*  11/5/2025
 prtObjInfo = {
     "Abele, Arija": {
         "room": "3"
@@ -1165,7 +2112,8 @@ prtObjInfo = {
     "Gllegos Figueroa, Adriana Socorro": {
         "room": "3"
     }
-}
+}*/
+
 function getForDAR() {
     let res = {};
     Object.keys(prtObjInfo).forEach(prtName => {
